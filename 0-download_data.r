10688082ac7c9ecf95d9548d9b4ff9f876545a57
neerajt <neerajtandon25@gmail.com>
2016-04-22 02:00:13 -0500

    deleted scripts from master branch
    Mistakenly committed scripts before data
    and didn't realize what a hassle that
    would cause for creating a clean data-
    only master branch. Manually removing
    the scripts here.

--- 0-download_data.r
+++
@@ -1,5 +0,0 @@
-
-download.file('http://hfdapp.houstontx.gov/311/311-Public-Data-Extract-2015-clean.txt',
-							destfile = '311-Public-Data-Extract-2015-clean.txt',
-							method = 'wget'
-							)
--- 1-remove_bad_lines.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-awk 'gsub(/\|/,"&")<29' 311-Public-Data-Extract-2015-clean.txt > 311-Public-Data-Extract-2015-ready.txt
